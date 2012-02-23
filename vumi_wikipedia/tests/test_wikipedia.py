42fb9361c50badf8243173f11bbbaaf9b13701c0
Jeremy Thurgood <jeremy@praekelt.com>
2012-02-23 15:45:55 +0200

    Better tests.

--- vumi_wikipedia/tests/test_wikipedia.py
+++ vumi_wikipedia/tests/test_wikipedia.py
@@ -1,21 +1,184 @@
 """Tests for vumi.demos.wikipedia."""
 
-from twisted.trial.unittest import TestCase
+import json
+from functools import wraps
+from pkg_resources import resource_stream
+
+from twisted.internet import reactor
 from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.internet.protocol import Protocol, Factory
+from twisted.trial.unittest import TestCase
 
 from vumi.tests.fake_amqp import FakeAMQPBroker
 from vumi.tests.utils import get_stubbed_worker
 from vumi.message import TransportUserMessage
-from vumi.demos.wikipedia import WikipediaWorker
+
+from vumi_wikipedia.wikipedia import WikipediaAPI, WikipediaWorker
 
 
-class WikipediaWorkerTestCase(TestCase):
+WIKIPEDIA_RESPONSES = json.load(
+    resource_stream(__name__, 'wikipedia_responses.json'))
+
+
+class FakeHTTP(Protocol):
+    def dataReceived(self, data):
+        request_line, body = self.parse_request(data)
+        response = self.handle_request(request_line, body)
+        self.transport.write(response.encode('utf-8'))
+        self.transport.loseConnection()
+
+    def parse_request(self, data):
+        headers, _, body = data.partition('\r\n\r\n')
+        headers = headers.splitlines()
+        request_line = headers.pop(0).rsplit(' ', 1)[0]
+        # print request_line
+        return request_line, body
+
+    def build_response(self, response_data):
+        lines = ["HTTP/1.1 %s" % (response_data['response_code'],)]
+        lines.extend(['', json.dumps(response_data['response_body'])])
+        return '\r\n'.join(lines)
+
+    def handle_request(self, request_line, body):
+        response_data = self.factory.response_data.get(request_line)
+        if not response_data:
+            self.factory.testcase.fail(
+                "Unexpected request: %s" % (request_line,))
+        self.factory.testcase.assertEqual(response_data["request_body"], body)
+        return self.build_response(response_data)
+
+
+class FakeHTTPTestCaseMixin(object):
+    @inlineCallbacks
+    def start_webserver(self, response_data):
+        factory = Factory()
+        factory.protocol = FakeHTTP
+        factory.response_data = response_data
+        factory.testcase = self
+        self.webserver = yield reactor.listenTCP(0, factory)
+        addr = self.webserver.getHost()
+        self.url = "http://%s:%s/" % (addr.host, addr.port)
+
+    def stop_webserver(self):
+        return self.webserver.loseConnection()
+
+
+def debug_api_call(func):
+    @wraps(func)
+    def wrapped_test(self):
+        self.wikipedia.PRINT_DEBUG = True
+        self.wikipedia.url = self.wikipedia.URL
+        return func(self)
+    return wrapped_test
+
+
+class WikipediaAPITestCase(TestCase, FakeHTTPTestCaseMixin):
+    timeout = 10
+
+    @inlineCallbacks
+    def setUp(self):
+        yield self.start_webserver(WIKIPEDIA_RESPONSES)
+        self.wikipedia = WikipediaAPI(self.url, False)
+
+    def tearDown(self):
+        return self.stop_webserver()
+
+    def assert_api_result(self, api_result_d, expected):
+        return api_result_d.addCallback(self.assertEqual, expected)
+
+    @inlineCallbacks
+    def test_search_success(self):
+        yield self.assert_api_result(
+            self.wikipedia.search('wikipedia', limit=3),
+            [u'Wikipedia', u'Wikip\xe9dia', u'Main Page'])
+        # And again with a different request and result limit
+        yield self.assert_api_result(
+            self.wikipedia.search('vumi', limit=2),
+            [u'Arambagh Utsab', u'Vulpia microstachys'])
+
+    @inlineCallbacks
+    def test_search_no_results(self):
+        yield self.assert_api_result(
+            self.wikipedia.search('ncdkiuagdqpowebjkcs', limit=3), [])
+
+    @inlineCallbacks
+    def test_get_sections_success(self):
+        yield self.assert_api_result(
+            self.wikipedia.get_sections('Triassic'), [
+                u'Dating and subdivisions',
+                u'Paleogeography',
+                u'Climate',
+                u'Life',
+                u'Coal',
+                u'Lagerst\xe4tten',
+                u'Late Triassic extinction event',
+                u'See also',
+                u'Notes',
+                u'References',
+                u'External links',
+                ])
+
+    @inlineCallbacks
+    def test_get_sections_none(self):
+        yield self.assert_api_result(
+            self.wikipedia.get_sections('Martin Lake'), [])
+
+    @inlineCallbacks
+    def test_get_content(self):
+        yield self.assert_api_result(
+            self.wikipedia.get_content('Dominion of New Zealand', 0),
+            u"The '''Dominion of New Zealand''' is the former name of the "
+            u"[[Realm of New Zealand]].\n\n[[Image:Dominion Day New "
+            u"Zealand.jpg|thumb|250px|right|[[William Plunket, 5th Baron "
+            u"Plunket|Lord Plunket]] declaring New Zealand a Dominion, 1907]]"
+            u"\nOriginally administered from [[New South Wales]], New Zealand "
+            u"became a direct British colony in 1841 and received a large "
+            u"measure of [[self-government]] following the [[New Zealand "
+            u"Constitution Act 1852]]. New Zealand chose not to take part in "
+            u"[[Australian Federati")
+
+    @inlineCallbacks
+    def test_get_content_shorter(self):
+        yield self.assert_api_result(
+            self.wikipedia.get_content(
+                'Dominion of New Zealand', 0, length_limit=200),
+            u"The '''Dominion of New Zealand''' is the former name of the "
+            u"[[Realm of New Zealand]].\n\n[[Image:Dominion Day New "
+            u"Zealand.jpg|thumb|250px|right|[[William Plunket, 5th Baron "
+            u"Plunket|Lord Plunket]] declar")
+
+    @inlineCallbacks
+    def test_get_content_infobox(self):
+        yield self.assert_api_result(
+            self.wikipedia.get_content('Hellinsia tripunctatus', 0),
+            u"{{Taxobox\n| name = \n| image = \t\n| image_width = 250px\n| "
+            u"image_caption = \n| regnum = [[Animal]]ia\n| phylum = "
+            u"[[Arthropod]]a\n| classis = [[Insect]]a\n| ordo = [[Lepidoptera"
+            u"]]\n| familia = [[Pterophoridae]]\n| subfamilia = \n| tribus = "
+            u"\n| genus = ''[[Hellinsia]]''\n| species = '''''H. tripunctatus"
+            u"'''''\n| binomial = ''Hellinsia tripunctatus''\n| "
+            u"binomial_authority = (Walsingham, 1881)<ref>["
+            u"http://www.afromoths.net/species/show/39458 Afro Moths]</ref>\n"
+            u"| synonyms = \n*''Aciptilus tripunctatus'' <small>Walsin")
+
+    @inlineCallbacks
+    def test_get_content_small(self):
+        yield self.assert_api_result(
+            self.wikipedia.get_content('Hellinsia tripunctatus', 1),
+            u'==References==\n{{Reflist}}\n\n[[Category:Hellinsia|tripunctatus'
+            u']]\n[[Category:Butterflies and moths of Africa]]\n[[Category:'
+            u'Insects of South Africa]]\n\n{{Oidaematophorini-stub}}\n\n'
+            u'[[vi:Hellinsia tripunctatus]]')
+
+
+class WikipediaWorkerTestCase(TestCase, FakeHTTPTestCaseMixin):
     transport_name = 'sphex'
 
     timeout = 10
 
     @inlineCallbacks
     def setUp(self):
+        yield self.start_webserver(WIKIPEDIA_RESPONSES)
         self.broker = FakeAMQPBroker()
         self._workers = []
         self.worker = yield self.get_worker()
@@ -24,6 +187,7 @@ class WikipediaWorkerTestCase(TestCase):
     def tearDown(self):
         for w in self._workers:
             yield w.stopWorker()
+        yield self.stop_webserver()
 
     @inlineCallbacks
     def get_worker(self, config=None):
@@ -31,6 +195,7 @@ class WikipediaWorkerTestCase(TestCase):
             config = {
                 'worker_name': 'wikitest',
                 'sms_transport': 'sphex',
+                'api_url': self.url,
                 }
         config.setdefault('transport_name', self.transport_name)
         worker = get_stubbed_worker(WikipediaWorker, config, self.broker)
@@ -63,42 +228,45 @@ class WikipediaWorkerTestCase(TestCase):
 
     @inlineCallbacks
     def test_happy_flow(self):
-        # TODO: Stub out the API calls.
         yield self.dispatch(self.mkmsg_in(None))
         self.assertEqual('What would you like to search Wikipedia for?',
                          self.get_dispatched_messages()[-1]['content'])
+
         yield self.dispatch(self.mkmsg_in('africa'))
         self.assertEqual('\n'.join([
-                    '1. Africa',
-                    '2. .africa',
-                    '3. African American',
-                    '4. North Africa',
-                    '5. Kenya',
-                    '6. Sub-Saharan Africa',
-                    '7. Africa (Roman province)',
-                    '8. African people',
-                    '9. Confederation of African Football']),
-                         self.get_dispatched_messages()[-1]['content'])
-        yield self.dispatch(self.mkmsg_in('1'))
-        self.assertEqual('\n'.join([
-                    '1. Africa',
-                    '2. Etymology',
-                    '3. History',
-                    '4. Geography',
-                    '5. Biodiversity',
-                    '6. Politics',
-                    '7. Economy',
-                    '8. Demographics',
-                    '9. Languages',
-                    '10. Culture',
-                    '11. Religion',
-                    '12. Territories and regions',
-                    '13. See also',
-                    '14. References',
-                    '15. Further reading',
-                    '16. External links',
+                    u'1. Africa',
+                    u'2. .africa',
+                    u'3. African American',
+                    u'4. North Africa',
+                    u'5. Kenya',
+                    u'6. Sub-Saharan Africa',
+                    u'7. Africa (Roman province)',
+                    u'8. African people',
+                    u'9. Confederation of African Football',
                     ]),
                          self.get_dispatched_messages()[-1]['content'])
+
+        yield self.dispatch(self.mkmsg_in('1'))
+        self.assertEqual('\n'.join([
+                    u'1. Africa',
+                    u'2. Etymology',
+                    u'3. History',
+                    u'4. Geography',
+                    u'5. Biodiversity',
+                    u'6. Politics',
+                    u'7. Economy',
+                    u'8. Demographics',
+                    u'9. Languages',
+                    u'10. Culture',
+                    u'11. Religion',
+                    u'12. Territories and regions',
+                    u'13. See also',
+                    u'14. References',
+                    u'15. Further reading',
+                    u'16. External links',
+                    ]),
+                         self.get_dispatched_messages()[-1]['content'])
+
         yield self.dispatch(self.mkmsg_in('2'))
         content = (
             u'==Etymology==\n\n[[Afri]] was a Latin name used to refer to the '
--- vumi_wikipedia/wikipedia.py
+++ vumi_wikipedia/wikipedia.py
@@ -1,4 +1,4 @@
-# -*- test-case-name: vumi.demos.tests.test_wikipedia -*-
+# -*- test-case-name: vumi_wikipedia.tests.test_wikipedia -*-
 
 import json
 from urllib import urlencode
@@ -8,7 +8,14 @@ from twisted.internet.defer import inlineCallbacks, returnValue
 from twisted.python import log
 
 from vumi.application import ApplicationWorker, SessionManager
-from vumi.utils import http_request, get_deploy_int
+from vumi.utils import http_request_full, get_deploy_int
+
+
+def either(*args):
+    for arg in args:
+        if arg is not None:
+            return arg
+    return None
 
 
 class WikipediaAPI(object):
@@ -22,17 +29,28 @@ class WikipediaAPI(object):
     # network traffic. However, Twisted only supports this easily from 11.1.
     GZIP = False
 
+    PRINT_DEBUG = False
+
+    def __init__(self, url=None, gzip=None):
+        self.url = either(url, self.URL)
+        self.gzip = either(gzip, self.GZIP)
+
     @inlineCallbacks
     def _make_call(self, params):
         params.setdefault('format', 'json')
-        url = '%s?%s' % (self.URL, urlencode(params))
+        url = '%s?%s' % (self.url, urlencode(params))
         headers = {
             'User-Agent': 'Vumi HTTP Request',
             }
-        if self.GZIP:
+        if self.gzip:
             headers['Accept-Encoding'] = 'gzip'
-        response = yield http_request(url, '', headers, method='GET')
-        returnValue(json.loads(response))
+        if self.PRINT_DEBUG:
+            print "\n=====\n\n%s /?%s\n" % ('GET', url.split('?', 1)[1])
+        response = yield http_request_full(url, '', headers, method='GET')
+        if self.PRINT_DEBUG:
+            print response.delivered_body
+            print "\n====="
+        returnValue(json.loads(response.delivered_body))
 
     @inlineCallbacks
     def search(self, query, limit=9):
@@ -77,7 +95,7 @@ class WikipediaAPI(object):
         returnValue(sections)
 
     @inlineCallbacks
-    def get_content(self, page_name, section_number):
+    def get_content(self, page_name, section_number, length_limit=500):
         """
         Return the content of a section of a page.
 
@@ -97,20 +115,7 @@ class WikipediaAPI(object):
                 })
 
         text = response['parse']['wikitext']['*']
-        returnValue(text[:500])
-
-
-def image(item, element):
-    el = item.find(element)
-    return getattr(el, 'attrib', {})
-
-
-def pretty_print_results(results, start=1):
-    """
-    Turn a list of results into an enumerate multiple choice list
-    """
-    return '\n'.join(['%s. %s' % (idx, result['text'])
-                      for idx, result in enumerate(results, start)])
+        returnValue(text[:length_limit])
 
 
 def format_options(options, start=1):
@@ -140,6 +145,10 @@ class WikipediaWorker(ApplicationWorker):
         self.session_manager = SessionManager(self.r_server,
             "%(worker_name)s:%(transport_name)s" % self.config,
             max_session_length=self.MAX_SESSION_LENGTH)
+
+        self.wikipedia = WikipediaAPI(
+            self.config.get('api_url', None),
+            self.config.get('accept_gzip', None))
 
         yield super(WikipediaWorker, self).startWorker()
 
@@ -174,7 +183,7 @@ class WikipediaWorker(ApplicationWorker):
     def process_message_searching(self, msg, session):
         query = msg['content'].strip()
 
-        results = yield WikipediaAPI().search(query)
+        results = yield self.wikipedia.search(query)
         if results:
             session['results'] = json.dumps(results)
             self.reply_to(msg, format_options(results), True)
@@ -206,7 +215,7 @@ class WikipediaWorker(ApplicationWorker):
             returnValue(session)
 
         session['page'] = selection
-        results = yield WikipediaAPI().get_sections(selection)
+        results = yield self.wikipedia.get_sections(selection)
         results = [selection] + results
         session['results'] = json.dumps(results)
         self.reply_to(msg, format_options(results), True)
@@ -219,7 +228,7 @@ class WikipediaWorker(ApplicationWorker):
         if not selection:
             session['state'] = None
             returnValue(session)
-        content = yield WikipediaAPI().get_content(
+        content = yield self.wikipedia.get_content(
             session['page'], int(msg['content'].strip()) - 1)
         ussd_cont = "%s...\n(Full content sent by SMS.)" % (content[:100],)
         self.reply_to(msg, ussd_cont, False)
