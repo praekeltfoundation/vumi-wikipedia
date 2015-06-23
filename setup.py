6e6970587fe22c5603084bc8797670f0b016234d
Jeremy Thurgood <jeremy@praekelt.com>
2015-06-23 10:34:42 +0200

    Release version 0.2.1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vumi-wikipedia',
-    version='0.2.1a0',
+    version='0.2.1',
     description='Vumi Wikipedia App',
     packages=find_packages(),
     include_package_data=True,
